01bb5ab711c23684e111ffc9fb7a9f42f58035c7
Simion <simion.okola@student.moringaschool.com>
2022-11-06 15:46:23 +0300

    ft-print review

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        
+    end
+
+    def print_all_reviews
+
+    end
+
+    def average_rating
+        
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews 
+
+    def favorite_product
+
+    end
+
+    def remove_reviews(product)
+        
+    end
 end
