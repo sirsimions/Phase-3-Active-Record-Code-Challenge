53b6a0cf3f0480584db9d58a8dd70d4a31d2fab2
Simion <simion.okola@student.moringaschool.com>
2022-11-06 20:15:43 +0300

    ft-favorite product

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ActiveRecord::Base
     has_many :products, through: :reviews 
 
     def favorite_product
-
+        fav= self.reviews.order(:star_rating)
+        fav.last
     end
 
     def remove_reviews(product)
-        
+
     end
 end
